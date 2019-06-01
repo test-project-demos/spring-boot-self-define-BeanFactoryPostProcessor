10e2e352f407f874e95333c690957a6a71d0aadf
Cnfn <cnfnss@gmail.com>
2019-06-01 21:02:17 +0800

    init

---
+++ src/main/kotlin/com/example/springbootselfdefineBeanFactoryPostProcessor/SpringBootSelfDefineBeanFactoryPostProcessorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootselfdefineBeanFactoryPostProcessor
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBootSelfDefineBeanFactoryPostProcessorApplication
+
+fun main(args: Array<String>) {
+	SpringApplication.run(SpringBootSelfDefineBeanFactoryPostProcessorApplication::class.java, *args)
+}
